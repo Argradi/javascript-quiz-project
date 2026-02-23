2f9e094be7ecb69ce88f12758747c696668fcbfc
Armando <argradi2003@gmail.com>
2026-02-23 17:02:23 +0100

    Question Done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,30 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text
+        this.choices = choices
+        this.answer = answer
+        this.difficulty = difficulty
+    }
 
-    // 2. shuffleChoices()
+    shuffleChoices() {
+        let arr = this.choices.slice()
+        let nums = []
+
+        // this.choices.forEach(function(element) {
+        //     Math.random() * this.choices.length
+        // })
+
+        for(let i = 0; i < this.choices.length; i++){
+            let n = Math.floor(Math.random() * this.choices.length)
+
+            while(nums.includes(n)){
+                n = Math.floor(Math.random() * this.choices.length)
+            }
+
+            nums.push(n)
+            this.choices[i] = arr[n]
+        }
+        
+        return this.choices
+    }
 }
